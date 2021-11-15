e556db3c3abc58870322d4ffe70634d7c9dccb9f
Akaczek <66081037+Akaczek@users.noreply.github.com>
2021-11-15 11:09:32 +0100

    Update poj_karta.py

--- poj_karta.py
+++ poj_karta.py
@@ -32,22 +32,30 @@ def dilation_erosion(x, n, kernel_size = 3, erode_iterations = 1, dilate_iterati
 
 
 if __name__ == '__main__':
-    img = cv.imread("cards_cropped_from_img/img_0.jpg")
+    img = cv.imread("cards_cropped_from_img/img_1.jpg")
 
     width = 1000
     height = 750
     dim = (width, height)
     img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
     imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, thresh = cv.threshold(imgray, 127, 255, 0)
+    ret, thresh = cv.threshold(imgray, 140, 255, 0)
     thresh = erosion_dilation(thresh, 5)
+    thresh = cv.erode(thresh, np.ones((3, 3), np.uint8), iterations=3)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     # cv.putText(img, "jakis tekst", (500,375), cv.FONT_HERSHEY_COMPLEX, 1.0, (255, 255, 255), 2)
     print(len(contours)) # number of detected countours, ~ ideally number of detected cards
     for i in range(len(contours)):
         M = cv2.moments(contours[i])
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+        try:
+            cX = int(M["m10"] / M["m00"])
+        except ZeroDivisionError:
+            cX = int(M["m10"] / 1)
+
+        try:
+            cY = int(M["m01"] / M["m00"])
+        except ZeroDivisionError:
+            cY = int(M["m01"] / 1)
         print(cX, cY)
         if cX < 145 and cY < 200:
             cv.drawContours(img, contours, i, GREEN ,3)
