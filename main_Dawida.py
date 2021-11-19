e2ac0d88fb0ad593065767a429c8121b6ae7259d
Akaczek <66081037+Akaczek@users.noreply.github.com>
2021-11-19 18:23:13 +0100

    Update main_Dawida.py

--- main_Dawida.py
+++ main_Dawida.py
@@ -1,29 +1,14 @@
-"""
-Card detection
-
-references:
-[1] C. Zheng, R. Green, ‘Playing Card Recognition Using Rotational Invariant Template Matching’, Proceedings of
-Image and Vision Computing New Zealand 2007, pp. 276-281, Hamilton, New Zealand, December 2007.
-[2] J. Pimentel and A. Bernardino, “A Comparison of Methods for Detection and Recognition of Playing Cards.”
-[3] Martins P., Reis L.P., Teófilo L. (2011) Poker Vision: Playing Cards and Chips Identification Based on Image
-Processing. In: Vitrià J., Sanches J.M., Hernández M. (eds) Pattern Recognition and Image Analysis. IbPRIA 2011.
-Lecture Notes in Computer Science, vol 6669. Springer, Berlin, Heidelberg
-[4] D. Ballard, “Generalizing the Hough transform to detect arbitrary shapes,” Pattern Recognition, vol. 13, no. 2,
-pp. 111–122, 1981.
-
-"""
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import OrderedDict
 import os
+import warnings
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 try:
     from cv2 import cv2 as cv
 except ImportError:
     pass
-from skimage import filters
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -35,6 +20,81 @@ MAGENTA = (255, 0, 255)
 YELLOW = (0, 255, 255)
 
 """ PRZETWARZANIE OBRAZU """
+
+
+class Board():
+    def __init__(self, filepath_):
+        self.filepath = filepath_
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.img = self.read_img(self.filepath)
+        self.detected_contours, self.detected_cards = self.detect_cards_on_board()
+        self.set = ''
+
+    def read_img(self, filepath):
+        img = cv.imread(filepath)
+        global WIDTH, HEIGHT
+        dim = (WIDTH, HEIGHT)
+        img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
+        return img
+
+    def detect_cards_on_board(self):
+        img = self.img
+        imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        imgray = cv.erode(imgray, np.ones((3, 3), np.uint8), iterations=1)
+        imgray = erosion_dilation(imgray, 10)
+        imgray = cv.GaussianBlur(imgray, (5, 5), 0, borderType=cv.BORDER_CONSTANT)
+        imgray = cv.GaussianBlur(imgray, (5, 5), 0, borderType=cv.BORDER_CONSTANT)
+
+        ret, thresh = cv.threshold(imgray, 140, 255, 0)
+        thresh = erosion_dilation(thresh, 10)
+
+        contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        print(len(contours))
+        cards = []
+        i = 0
+        while i < len(contours):
+            c = contours[i]
+            _, size, _ = cv.minAreaRect(c)
+            aspect_ratio = float(size[0]) / size[1]
+            if cv.contourArea(c) > 20000:
+                img_cropped, _ = crop_image(img, c)
+                card = Card(c, img_cropped)
+                card.nadajFigure()
+                card.nadajKolor()
+                cards.append(card)
+                i += 1
+            else:
+                contours = np.delete(contours, i)
+
+
+        # Displaying the results
+        # cv.drawContours(img, contours, -1, GREEN, 3)
+        # cv.imshow('', img)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
+
+        return contours, cards
+
+    def label_cards(self):
+        img = self.img
+        assert self.detected_contours is not None
+        for i, c in enumerate(self.detected_contours):
+            cv.drawContours(img, self.detected_contours, i, MAGENTA, 3)
+            x, y, _, _ = cv.boundingRect(c)
+            label = "{} {}".format(self.detected_cards[i].figura, self.detected_cards[i].kolor)
+            translated_label = ' '.join(
+                [card_label_names.get(i, i) for i in label.split()])  # tlumaczenie znakow A, K, Q na As, Krol, Dama
+            cv.putText(img, translated_label, (x, y - 10), cv.FONT_HERSHEY_COMPLEX, 0.8, WHITE, 2)
+        show(img)
+
+    def detectSet(self):
+        figury = []
+        kolory = []
+        for c in self.detected_cards:
+            figury.append(c.figura)
+            kolory.append(c.kolor)
+        print(figury, kolory)
 
 
 def erosion_dilation(x, n, kernel_size=3, erode_iterations=1, dilate_iterations=1):
@@ -51,13 +111,6 @@ def dilation_erosion(x, n, kernel_size=3, erode_iterations=1, dilate_iterations=
         x = cv.dilate(x, matrix_of_ones, iterations=dilate_iterations)
         x = cv.erode(x, matrix_of_ones, iterations=erode_iterations)
     return x
-
-
-def processed(image):
-    grayscaled = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    blurred = cv.GaussianBlur(grayscaled, (5, 5), 0)
-    thresholded = cv.Canny(blurred, threshold1=200, threshold2=200)
-    return thresholded
 
 
 # https://stackoverflow.com/questions/37177811/crop-rectangle-returned-by-minarearect-opencv-python
@@ -97,37 +150,155 @@ def getSubImage(src, rect):
 
 """ PRZETWARZANIE KART """
 
-
-def card_rank(id_card_rank):
-    return {
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9,
-        "10": 10,
-        "Walet": 11,
-        "Dama": 12,
-        "Król": 13,
-        "As": 14,
-    }[id_card_rank]
+card_label_names = {
+    'A': 'As',
+    'K': 'Krol',
+    'Q': 'Dama',
+    'J': 'Walet',
+}
 
 
-def card_suit(id_card_suit):
-    return {
-        "Trefl": 1,
-        "Karo": 2,
-        "Kier": 3,
-        "Pik": 4,
-    }[id_card_suit]
+def wczytaj(img):
+    thresh = cv.imread(img)
+    thresh = cv.cvtColor(thresh, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(thresh, 140, 255, 0)
+    return thresh
+
+
+maski_figur = {
+    "9": wczytaj("template/9.jpg"),
+    "1": wczytaj("template/1.jpg"),
+    "0": wczytaj("template/0.jpg"),
+    "J": wczytaj("template/J.jpg"),
+    "Q": wczytaj("template/Q.jpg"),
+    "K": wczytaj("template/K.jpg"),
+    "A": wczytaj("template/A.jpg")
+}
+
+maski_koloru = {
+    "kier": wczytaj("template/kier.jpg"),
+    "karo": wczytaj("template/karo.jpg"),
+    "pik": wczytaj("template/pik.jpg"),
+    "trefl": wczytaj("template/trefl.jpg"),
+}
 
 
 class Card:
-    def __init__(self):
-        self.contour = []
+    def __init__(self, contour_, img_):
+        self.contour = contour_
+        self.img = img_
+        self.figura = ''
+        self.kolor = ''
+        self.pozycja = None
+
+    def nadajFigure(self):
+        width = 500
+        height = 500
+        dim = (width, height)
+        img = cv.resize(self.img, dim, interpolation=cv.INTER_AREA)
+        imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(imgray, 140, 255, 0)
+        thresh = erosion_dilation(thresh, 5)
+        thresh = cv.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # cv.putText(img, "jakis tekst", (500,375), cv.FONT_HERSHEY_COMPLEX, 1.0, (255, 255, 255), 2)
+        # number of detected countours, ~ ideally number of detected cards
+        flaga = 0
+        for i in range(len(contours)):
+            M = cv.moments(contours[i])
+            try:
+                cX = int(M["m10"] / M["m00"])
+            except ZeroDivisionError:
+                cX = int(M["m10"] / 1)
+
+            try:
+                cY = int(M["m01"] / M["m00"])
+            except ZeroDivisionError:
+                cY = int(M["m01"] / 1)
+            if cX < 100 and cY < 70 and cX != 0 and cY != 0 and flaga == 0 and hierarchy[0][i][3] == 0:
+                rect = cv.boundingRect(contours[i])
+                koord = [rect[0], rect[1]]
+                size = [rect[2], rect[3]]
+                wyciecie = cv.getRectSubPix(img, size, (koord[0] + size[0] // 2, koord[1] + size[1] // 2))
+                cv.drawContours(img, contours, i, MAGENTA, 3)
+                flaga = 1
+
+        wyciecie = cv.resize(wyciecie, (60, 80), interpolation=cv.INTER_AREA)
+        wyciecie = cv.cvtColor(wyciecie, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(wyciecie, 140, 255, 0)
+        thresh = cv.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+
+
+        najlepszy = 10000
+        naj_figura = "9"
+        for figura, maska in maski_figur.items():
+            diff_img = cv.absdiff(thresh, maska)
+            rank_diff = int(np.sum(diff_img) / 255)
+            if rank_diff < najlepszy:
+                najlepszy = rank_diff
+                naj_figura = figura
+
+        if naj_figura in ["1", "0"]:
+            self.figura = "10"
+        else:
+            self.figura = naj_figura
+
+    def nadajKolor(self):
+        width = 500
+        height = 500
+        dim = (width, height)
+        img = cv.resize(self.img, dim, interpolation=cv.INTER_AREA)
+        imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(imgray, 140, 255, 0)
+        thresh = erosion_dilation(thresh, 5)
+        thresh = cv.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # cv.putText(img, "jakis tekst", (500,375), cv.FONT_HERSHEY_COMPLEX, 1.0, (255, 255, 255), 2)
+        # number of detected countours, ~ ideally number of detected cards
+        flaga = 0
+        for i in range(len(contours)):
+            M = cv2.moments(contours[i])
+            try:
+                cX = int(M["m10"] / M["m00"])
+            except ZeroDivisionError:
+                cX = int(M["m10"] / 1)
+
+            try:
+                cY = int(M["m01"] / M["m00"])
+            except ZeroDivisionError:
+                cY = int(M["m01"] / 1)
+            if cX < 100 and 90 < cY < 150 and cX != 0 and cY != 0 and hierarchy[0][i][3] == 0:
+                rect = cv.boundingRect(contours[i])
+                koord = [rect[0], rect[1]]
+                size = [rect[2], rect[3]]
+                wyciecie = cv.getRectSubPix(img, size, (koord[0] + size[0] // 2, koord[1] + size[1] // 2))
+                cv.drawContours(img, contours, i, MAGENTA, 3)
+                flaga = 1
+
+        wyciecie = cv.resize(wyciecie, (60, 80), interpolation=cv.INTER_AREA)
+        wyciecie = cv.cvtColor(wyciecie, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(wyciecie, 140, 255, 0)
+        # thresh = cv.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+
+        najlepszy = 10000
+        naj_kolor = "kier"
+        for kolor, maska in maski_koloru.items():
+            diff_img = cv.absdiff(thresh, maska)
+            rank_diff = int(np.sum(diff_img) / 255)
+            if rank_diff < najlepszy:
+                najlepszy = rank_diff
+                naj_kolor = kolor
+
+        self.kolor = naj_kolor
+
+    def getFigura(self):
+        return self.figura
+
+    def getKolor(self):
+        return self.kolor
+
+    def getPozycja(self):
+        return self.pozycja
 
 
 def show(image):
@@ -136,73 +307,31 @@ def show(image):
     cv.destroyAllWindows()
 
 
-def card_contouring(filepath):
-    img = cv.imread(filepath)
-    global WIDTH, HEIGHT
-    dim = (WIDTH, HEIGHT)
-    img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-    imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    imgray = cv.erode(imgray, np.ones((3,3), np.uint8), iterations=1)
-    imgray = erosion_dilation(imgray, 10)
-    imgray = cv.GaussianBlur(imgray, (5, 5), 0, borderType=cv.BORDER_CONSTANT)
-    imgray = cv.GaussianBlur(imgray, (5, 5), 0, borderType=cv.BORDER_CONSTANT)
+def crop_image(img, contour):
+    rect = cv.minAreaRect(contour)
+    img_cropped = getSubImage(img, rect)
+    if img_cropped.shape[0] < img_cropped.shape[1]:
+        img_cropped = cv.rotate(img_cropped, cv.ROTATE_90_CLOCKWISE)
 
-    ret, thresh = cv.threshold(imgray, 127, 255, 0)
-    thresh = erosion_dilation(thresh, 10)
-
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-    for i, c in enumerate(contours):
-        img_copy = img.copy()
-        # get the bounding rect
-        # x, y, w, h = cv.boundingRect(c)
-        rect = cv.minAreaRect(c)
-        img_cropped = getSubImage(img_copy, rect)
-        if(img_cropped.shape[0] < img_cropped.shape[1]):
-            img_cropped = cv.rotate(img_cropped, cv.ROTATE_90_CLOCKWISE)
-        # to save the images
-        # cv.imwrite('./cards_cropped_from_img/img_{}.jpg'.format(i), img_cropped)
-        # cv.imwrite('./cards_cropped_from_img/img_{}.jpg'.format(i), img[y:y+h,x:x+w])
-
-    # cv.putText(img, "jakis tekst", (500,375), cv.FONT_HERSHEY_COMPLEX, 1.0, WHITE, 2)
-
-    print(len(contours))  # number of detected countours, ~ ideally number of detected cards
-    cv.drawContours(img, contours, -1, GREEN, 3)
-
-    # Displaying the results
-    cv.imshow('', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-    # TODO: switch to matplotlib for showing multiple images
-    return img
+    return img_cropped, rect
 
 
-def main(path):
-    images = OrderedDict({})
-    for (root, _, filenames) in os.walk(path):
-        for filename in filenames:
-            images[filename] = card_contouring(root + filename)
-
-    """ Ponizszy fragment laczy i wyswietla obrazki horyzontalnie"""
-    # tmp = np.hstack((img, img))
-    # cv.imshow("Obrazki na horyzoncie", tmp)
-    for imagename in images.keys():
-        show(images[imagename])
-
-    # TODO: Dipslay multiple images https://answers.opencv.org/question/175912/how-to-display-multiple-images-in-one-window/
+WIDTH = 750
+HEIGHT = 750
+PATH = './karty/'
 
 
-def process_selected_images(path, selected_images):
+def main():
+    selected_images = ['flush.jpg', 'four_of_kind.jpg', 'full.jpg', 'hight_card.jpg', 'pair.jpg', 'pair2.jpg',
+                       'royal_flush.jpg', 'small_strit.jpg', 'three_of_kind.jpg', 'two_pairs.jpg', 'two_pairs2.jpg']
+
     for filename in selected_images:
-        card_contouring(path + filename)
+        board = Board(PATH + filename)  # detekcja kart w konstruktorze
+        board.label_cards()  # tutaj jest odkomentowany show(img)
+        board.detectSet()
+        print('----------------------------------')
+
 
 
 if __name__ == '__main__':
-    WIDTH = 750
-    HEIGHT = 750
-    PATH = './karty/'
-
-    selected_images = ['dwie_pary.jpg', 'dwie.jpg', 'as_pik.jpg', 'krol_karo.jpg']
-    process_selected_images(PATH,
-                            selected_images)  # FIXME: processes only the first element in the list, due to cv.waitKey(0) and cv.destroyAllWindows()
+    main()
